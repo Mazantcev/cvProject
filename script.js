0e9816ba08fed523f1c9faa05625778102a97890
Vlad <100831532+Mazantcev@users.noreply.github.com>
2024-04-11 14:58:24 +0700

    Make my first cv page, add 1 js function on skill button and add some responsive design feauters

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const btn = document.getElementById("skillButton");
+const showSkill = document.getElementById("skillsIcons");
+
+btn.onclick = function () {
+  if (showSkill.style.display = 'none') {
+    showSkill.style.display = 'flex';
+  }
+};
